4b4ff625e8f209dab1c33c422deb30b4a7c56cfd
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2019-12-02 16:55:57 -0800

    re-added initial UI fade in

--- TurboCop/app.js
+++ TurboCop/app.js
@@ -1,10 +1,34 @@
-const { ipcRenderer } = require('electron');
+// Copyright 2019 Dmitry Pleshkov. All Rights Reserved.
+
+const {
+    ipcRenderer
+} = require('electron');
 const $ = require("jquery");
 const AES = require("crypto-js/aes");
 const SHA256 = require("crypto-js/sha256");
 require("popper.js");
 require("bootstrap");
 
-class MainMenuAgent() {
-    
+class MenuAgent {
+    static openMainMenu() {
+        $(".settings-menu").fadeOut(500);
+        setTimeout(function() {
+            $(".main-menu").fadeIn(500);
+            $("#statusMessage").text("What would you like to do?")
+        }, 500)
+    }
+    static openSettingsMenu() {
+
+    }
+    static performInitialFadeIn() {
+        $("#turboCopHeading").fadeIn(500);
+        MenuAgent.openMainMenu();
+    }
+    static performModalPrompt(description, callback) {
+
+    }
 }
+
+$(document).ready(function() {
+    MenuAgent.performInitialFadeIn();
+})
